6472f864aa03180461bea7d6ca5b563eab1030eb
Abhishek Choudhary <itzabhishek5127@gmail.com>
2025-11-30 23:24:03 +0530

    changes in server

--- server/src/app.js
+++ server/src/app.js
@@ -28,5 +28,6 @@ app.use("/api/resume", resumeRoutes);
 app.use("/api/enhance", enhanceRoutes);
 app.use("/api/summary", summaryRoutes);
 app.use("/api/project", projectRoutes);
+app.use("/api/predict-skills", enhanceRoutes);
 
 export default app;
