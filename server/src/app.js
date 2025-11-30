90e771bcc2e078f0e584b24832297a7daceb4077
Abhishek Choudhary <itzabhishek5127@gmail.com>
2025-11-30 19:49:41 +0530

    updating server according to render

--- server/src/app.js
+++ server/src/app.js
@@ -8,14 +8,25 @@ import projectRoutes from "./routes/project.route.js";
 
 const app = express();
 
-app.use(cors());
+// ALLOW YOUR FRONTEND DOMAIN ONLY
+app.use(
+  cors({
+    origin: "https://ai-resume-builder-5koy.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+// For preflight CORS checks
+app.options("*", cors());
+
 app.use(express.json());
 
+// ROUTES
 app.use("/api/resume", resumeRoutes);
-app.use("/api/enhance", enhanceRoutes); 
-app.use("/api/summary",summaryRoutes);
+app.use("/api/enhance", enhanceRoutes);
+app.use("/api/summary", summaryRoutes);
 app.use("/api/project", projectRoutes);
-
-
 
 export default app;
